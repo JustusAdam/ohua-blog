6e4549d300e6d8bd490936266b91fcc8d3c25ac5
Justus Adam <dev@justus.science>
2016-01-31 12:18:50 +0100

    Removed `this`
    The new macro does not require it anymore

--- src/pre/clojure/ohua_blog/process.clj
+++ src/pre/clojure/ohua_blog/process.clj
@@ -1,32 +1,31 @@
 (ns ohua-blog.process
-  (:import (com.ohua.lang Function))
   (:require [com.ohua.util.gen :refer [function aot-function]]))
 
-(aot-function processTopics ^Iterable [this ^Iterable posts]
+(aot-function processTopics ^Iterable [^Iterable posts]
   (frequencies (map (fn [a] (.getTopic a)) posts)))
 
-(aot-function orderPosts [this ^Iterable views]
+(aot-function orderPosts [^Iterable views]
   (into [] (take 5 (sort-by last > views))))
 
-(aot-function zip ^Iterable [this ^Iterable a ^Iterable b]
+(aot-function zip ^Iterable [^Iterable a ^Iterable b]
   (into [] (map vector a b)))
 
-(aot-function unzipFirst [this l]
+(aot-function unzipFirst [l]
   (into [] (map (fn [[a b]] a) l)))
 
-(aot-function takeLatest [this ^Iterable posts]
+(aot-function takeLatest [^Iterable posts]
   (let [res (take 5 (sort-by (fn [post] (.getDate post)) posts))]
     res))
 
-(aot-function getId [this ^Object o]
+(aot-function getId [^Object o]
   (.getId o))
 
-(aot-function zip3 [this ^Iterable a ^Iterable b ^Iterable c]
+(aot-function zip3 [^Iterable a ^Iterable b ^Iterable c]
   (into [] (map vector a b c)))
 
-(aot-function getIds [this ^Iterable a]
+(aot-function getIds [^Iterable a]
   (map #(.getId %) a))
 
-(aot-function p [this a]
+(aot-function p [a]
   (println a)
   a)
--- src/pre/clojure/ohua_blog/render.clj
+++ src/pre/clojure/ohua_blog/render.clj
@@ -3,28 +3,29 @@
   (require [com.ohua.util.gen :refer [aot-function]]))
 
 
-(aot-function renderTopics ^String  [this ^Iterable topics]
+(aot-function renderTopics ^String  [^Iterable topics]
   (str "Topics:\n" (join "\n" (map str topics))))
 
-(aot-function renderPostList ^String [this ^Iterable posts]
+(aot-function renderPostList ^String [^Iterable posts]
   (join "\n" (map #(str "Post " (.getId (first %))) posts)))
 
-(aot-function renderSidePane ^String [this ^Object posts ^Object topics]
+(aot-function renderSidePane ^String [^Object posts ^Object topics]
   (str "Side panel\n" posts "\n" topics))
 
-(aot-function renderMainPanel ^String [this ^Iterable posts]
-  (str "Main panel\n" (renderPostList this posts)))
+(aot-function renderMainPanel ^String [^Iterable posts]
+  (str "Main panel\n" (renderPostList posts)))
 
-(aot-function renderPosts ^String [this ^Iterable posts]
-  (renderPostList this posts))
+(aot-function renderPosts ^String [^Iterable posts]
+  (renderPostList posts))
 
-(aot-function renderPage ^String [this ^Object pane ^Object content]
+(aot-function renderPage ^String [^Object pane ^Object content]
   (str pane "\n" content))
 
-(aot-function renderPopularPosts ^String [this ^Iterable posts]
+(aot-function renderPopularPosts ^String [^Iterable posts]
   (join "\n" (map (fn [[views info content]] (str " Views: " views "Content:" content)) posts)))
 ;
 ; (use 'clojure.pprint)
 ; (use 'clojure.walk)
 ;
-; (pprint (macroexpand '(function testGen [this a b] a)))
+; (pprint (macroexpand '(aot-function renderPage ^String [^Object pane ^Object content]
+;                                     (str pane "\n" content))))
